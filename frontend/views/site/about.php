be5607e6cdfa4aee218f407800a154a244c94a01
Dev-Kings <kingasiadavid41@gmail.com>
2021-12-06 09:43:50 +0300

    img upload

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -7,10 +7,19 @@ use yii\helpers\Html;
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<?php
+
+$url = Yii::getAlias("@web") . '/../';
+
+?>
+
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>This is the About page. You may modify the following file to customize its content:</p>
 
+    <?= Html::img('@web/uploads/House On Sale.jpg', ['alt'=>'some', 'class'=>'thing']);?>
+    
+    <br>
     <code><?= __FILE__ ?></code>
 </div>
